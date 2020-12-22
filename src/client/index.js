de2d13be0a6b3d9fb9adcd324f73aec73a2794f7
Diego Dmitry <diegodmitry@users.noreply.github.com>
2020-12-22 09:53:51 -0300

    The webpack version and terser-plugin version specified in the package.json files are not compatible. Then, I changed the terser-plugin version to a compatible one (v4.2.3).

--- src/client/index.js
+++ src/client/index.js
@@ -2,7 +2,7 @@
 import { checkForURL } from './js/urlChecker'
 import { handleSubmit } from './js/formHandler'
 
-console.log(checkForName);
+// console.log(checkForName);
 
 //alert("I EXIST")
 //console.log("CHANGE!!");
